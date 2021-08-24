8074e79e11de6a610feb3cc211167da5f53c4bb2
menashekoren <menashekoren@gmail.com>
2021-08-24 10:54:19 +0300

    Feat: Complete javascript popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,26 @@
-function initPopUp() {
-  let form = document.querySelector(".edit-form");
+let form = document.querySelector(".edit-form");
+let edit = document.querySelector(".edit__icon");
+let close = document.querySelector(".form__close");
 
-  let edit = document.querySelector(".edit__icon");
-
-  let close = document.querySelector(".form__close");
-
-
-  function closePopUpClick() {
-    form.classList.remove('edit-form_opened');
-  }
-
-  close.addEventListener("click", closePopUpClick);
-
-  function openPopUpClick() {
-    form.classList.add('edit-form_opened');
-  }
-
-  edit.addEventListener("click", openPopUpClick);
+function closePopUp() {
+  form.classList.remove('edit-form_opened');
 }
+close.addEventListener("click", closePopUp);
 
-initPopUp();
-
-let formElement = document.querySelector(".form__field");// Use the querySelector() method
+function openPopUp() {
+  form.classList.add('edit-form_opened');
+}
+edit.addEventListener("click", openPopUp);
+let formElement = document.querySelector(".form__field");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  let nameInput = document.querySelector('.field__name');// Use querySelector()
-  let jobInput = document.querySelector('.field__profession');// Use querySelector()
-
-
-  document.querySelector(".name__default").textContent = nameInput.value;
-  document.querySelector(".profile-info__profession").textContent = jobInput.value;
+  let nameInput = document.querySelector('.field__name');
+  let jobInput = document.querySelector('.field__profession');
+  document.querySelector(".name__default")
+    .textContent = nameInput.value;
+  document.querySelector(".profile-info__profession")
+    .textContent = jobInput.value;
 }
-
-let form = document.querySelector(".edit-form");
-function closePopUp() {
-    form.classList.remove('edit-form_opened');
-  }
-
-
 formElement.addEventListener('submit', handleFormSubmit);
 formElement.addEventListener('submit', closePopUp);
