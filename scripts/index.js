a90ebb7a157030e963a9fd834373b3c1c7a503b4
menashekoren <menashekoren@gmail.com>
2021-08-25 11:15:14 +0300

    Bug: Popup won't save

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,16 @@ function openPopUp() {
   form.classList.add('edit-form_opened');
 }
 edit.addEventListener("click", openPopUp);
+
+
 let formElement = document.querySelector(".form__field");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let nameInput = document.querySelector('#name');
-  let jobInput = document.querySelector('#profession');
-  document.querySelector(".name__default")
-    .textContent = nameInput.value;
-  document.querySelector(".profile-info__profession")
-    .textContent = jobInput.value;
+  let nameInput = document.querySelector('.field__input_type_name');
+  let jobInput = document.querySelector('.field__input_type_profession');
+  document.querySelector(".name__default").textContent = nameInput.value;
+  document.querySelector(".profile-info__profession").textContent = jobInput.value;
 }
 formElement.addEventListener('submit', handleFormSubmit);
 formElement.addEventListener('submit', closePopUp);
