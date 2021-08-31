699a7deff9622a7ec3030c93dd43aab08debfb4d
menashekoren <menashekoren@gmail.com>
2021-09-01 02:28:53 +0300

    Hotfix: Fix all first corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
 let form = document.querySelector(".form");
 let edit = document.querySelector(".edit-button");
 let close = document.querySelector(".form__close");
+let formElement = document.querySelector(".form__field");
+let userNameValue = document.querySelector('.edit-name');
+let userJobValue = document.querySelector('.profile-info__profession');
+let nameInput = document.querySelector(".field-input_type_name");
+let jobInput = document.querySelector(".field-input_type_profession");
 
 function closePopUp() {
   form.classList.remove('form_opened');
 }
-close.addEventListener("click", closePopUp);
 
 function openPopUp() {
   form.classList.add('form_opened');
+  let userName = userNameValue.textContent
+  nameInput.value = userName;
+  let userJob = userJobValue.textContent;
+  jobInput.value = userJob;
 }
-edit.addEventListener("click", openPopUp);
-
-
-let formElement = document.querySelector(".form__field");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let nameInput = document.querySelector(".field-input_type_name");
-  let jobInput = document.querySelector(".field-input_type_profession");
   document.querySelector(".edit-name").textContent = nameInput.value;
   document.querySelector(".profile-info__profession").textContent = jobInput.value;
+  closePopUp();
 }
+
 formElement.addEventListener('submit', handleFormSubmit);
-formElement.addEventListener('submit', closePopUp);
+edit.addEventListener("click", openPopUp);
+close.addEventListener("click", closePopUp);
