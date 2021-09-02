dfa910a04a7dc96c9e767cf9243aa936d4de9f03
menashekoren <menashekoren@gmail.com>
2021-09-02 19:18:27 +0300

    Feat: Add initial cards js

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,44 @@
-let form = document.querySelector(".form");
-let edit = document.querySelector(".edit-button");
-let close = document.querySelector(".form__close");
-let formElement = document.querySelector(".form__field");
-let userNameValue = document.querySelector('.edit-name');
-let userJobValue = document.querySelector('.profile-info__profession');
-let nameInput = document.querySelector(".field-input_type_name");
-let jobInput = document.querySelector(".field-input_type_profession");
+const initialCards = [
+  {
+    name:  "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
 
-function closePopUp() {
-  form.classList.remove('form_opened');
-}
+const templateCardItem = document.querySelector('.card-template').content.querySelector('.card');
+const list = document.querySelector('.cards');
 
-function openPopUp() {
-  form.classList.add('form_opened');
-  let userName = userNameValue.textContent
-  nameInput.value = userName;
-  let userJob = userJobValue.textContent;
-  jobInput.value = userJob;
-}
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  document.querySelector(".edit-name").textContent = nameInput.value;
-  document.querySelector(".profile-info__profession").textContent = jobInput.value;
-  closePopUp();
-}
 
-formElement.addEventListener('submit', handleFormSubmit);
-edit.addEventListener("click", openPopUp);
-close.addEventListener("click", closePopUp);
+initialCards.forEach(function (cardData) {
+  const cardItem = templateCardItem.cloneNode(true);
+
+  const title = cardItem.querySelector('.card__location');
+  const image = cardItem.querySelector('.card__image');
+
+  title.textContent = cardData.name;
+  image.src = cardData.link;
+
+  list.append(cardItem)
+
+});
