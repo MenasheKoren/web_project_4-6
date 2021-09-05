12dffe3e6cce71d23e7b3ed1ca098652ea006d25
menashekoren <menashekoren@gmail.com>
2021-09-05 15:00:10 +0300

    Feat: Update popup js and classes

--- scripts/index.js
+++ scripts/index.js
@@ -29,12 +29,14 @@ const templateCardItem = document.querySelector('.card-template').content.queryS
 const list = document.querySelector('.card-list');
 
 // modals
-const editModal = document.querySelector('.form_type_edit');
-const addCardModal = document.querySelector('.form_type_add-card');
+const editModal = document.querySelector('.popup_type_edit');
+const addCardModal = document.querySelector('.popup_type_add-card');
+const imageModal = document.querySelector('.popup_type_image');
 
 // close buttons
-const editModalCloseButton = editModal.querySelector('.form__close');
-const addCardModalCloseButton = addCardModal.querySelector('.form__close');
+const editModalCloseButton = editModal.querySelector('.popup__close');
+const addCardModalCloseButton = addCardModal.querySelector('.popup__close');
+const imageModalCloseButton = imageModal.querySelector('.popup__close');
 
 // open modal buttons
 const editProfileButton = document.querySelector('.edit-button');
@@ -49,17 +51,20 @@ const cardLinkInput = addCardModal.querySelector('.field-input_type_card-link');
 
 
 // forms
-const editForm = editModal.querySelector('.form__field');
-const addCardForm = addCardModal.querySelector('.form__field');
+const editForm = editModal.querySelector('.popup__form');
+const addCardForm = addCardModal.querySelector('.popup__form');
 
+// image modal elements
+// const imagePopupFigure = document.querySelector()
 
-function toggleModal(form) {
-  form.classList.toggle('form_opened')
+// functions
+function toggleModal(popup) {
+  popup.classList.toggle('popup_opened')
 };
 
 addCardForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  console.log(`123`, cardNameInput.value, cardLinkInput.value);
+  console.log(cardNameInput.value, cardLinkInput.value);
 
   generateCard({name: cardNameInput.value, link: cardLinkInput.value});
   toggleModal(addCardModal);
@@ -68,20 +73,24 @@ addCardForm.addEventListener('submit', (evt) => {
 
 
 editProfileButton.addEventListener('click', () => {
-  toggleModal(editModal)
+  toggleModal(editModal);
 });
 
 editModalCloseButton.addEventListener('click', () => {
-  toggleModal(editModal)
+  toggleModal(editModal);
 });
 
 addCardButton.addEventListener('click', () => {
   addCardForm.reset();
-  toggleModal(addCardModal)
+  toggleModal(addCardModal);
 });
 
 addCardModalCloseButton.addEventListener('click', () => {
-  toggleModal(addCardModal)
+  toggleModal(addCardModal);
+});
+
+imageModalCloseButton.addEventListener('click', () => {
+  toggleModal(imageModal);
 });
 
 function generateCard(cardData) { // {name, link}
@@ -89,10 +98,31 @@ function generateCard(cardData) { // {name, link}
 
   const title = cardItem.querySelector('.card__location');
   const image = cardItem.querySelector('.card__image');
+  const removeButton = cardItem.querySelector('.card__remove');
+  const likeButton = cardItem.querySelector('.card__like');
 
   title.textContent = cardData.name;
   image.src = cardData.link;
   image.alt = cardData.name;
+
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('card__like_filled')
+  });
+
+  removeButton.addEventListener('click', () => {
+    cardItem.remove();
+  });
+
+  image.addEventListener('click', () => {
+    toggleModal(imageModal);
+    // fill image src => el.src = cardData.link
+    image.src = cardData.link;
+    console.log(image.src);
+    //  fill caption => el.textContent = cardData.name
+    title.textContent = cardData.name;
+    console.log(title.textContent);
+  });
+
 
   list.append(cardItem)
 };
