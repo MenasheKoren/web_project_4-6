022b2050c837588650eade4c50f18376f5695211
menashekoren <menashekoren@gmail.com>
2021-09-12 18:06:37 +0300

    Feat: Remove extra files

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,45 +2,33 @@ function showError(input) {
   const error = input.validationMessage
   const errorElement = document.querySelector(`#${input.id}-error`)
   errorElement.textContent = error
-
-  //input.classList.add(settings.inputErrorClass)
-  //settings.inputSelector.invalid = true
 }
+
 function hideError(input) {
   const errorElement = document.querySelector(`#${input.id}-error`)
   errorElement.textContent = ''
-  //input.classList.remove(settings.inputErrorClass)
-  //settings.inputSelector.invalid = false
 }
 
 
 function checkValidity(input) {
-  !input.validity.valid ?
-    showError(input)
-    :
-    hideError(input)
+  !input.validity.valid ? showError(input) : hideError(input);
 }
 
 function toggleButtonState(inputs, button) {
 
   const isFormValid = inputs.every(input => input.validity.valid)
-  !isFormValid ?
-    button.disabled = true
-    //button.classList.toggle(settings.inactiveButtonClass))
-    :
-    button.disabled = false
-    //button.classList.toggle(settings.inactiveButtonClass))
+  !isFormValid ? button.disabled = true : button.disabled = false
 }
 
 function enableValidation(settings) {
   const {formSelector, inputSelector, submitButtonSelector} = settings
-  const forms = [...document.querySelectorAll(settings.formSelector)];
+  const forms = [...document.querySelectorAll(formSelector)];
 
   forms.forEach(form => {
     form.addEventListener('submit', (evt) => evt.preventDefault());
 
-    const inputs = [...form.querySelectorAll(settings.inputSelector)];
-    const button = form.querySelector(settings.submitButtonSelector)
+    const inputs = [...form.querySelectorAll(inputSelector)];
+    const button = form.querySelector(submitButtonSelector)
 
     inputs.forEach(input => {
       input.addEventListener('input', () => {
@@ -54,10 +42,7 @@ function enableValidation(settings) {
 const config = {
   formSelector: ".popup__form",
   inputSelector: ".field-input",
-  submitButtonSelector: ".popup__save",
-  inactiveButtonClass: "disabled",
-  inputErrorClass: "invalid",
-  errorClass: "popup__error_visible",
+  submitButtonSelector: ".popup__save"
 }
 
 enableValidation(config);
