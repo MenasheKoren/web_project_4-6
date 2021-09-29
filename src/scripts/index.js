0dbc82cfbd854fbc107ba950e57159062061b0bc
menashekoren <menashekoren@gmail.com>
2021-09-29 15:34:57 +0300

    Feat: Install webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,6 @@ import {Card} from './Card.js';
 import {FormValidator} from './FormValidator.js';
 import {toggleModal} from './utils.js';
 
-import logoSrc from "../images/logo.svg";
-
-const headerLogo = document.getElementById('header-logo');
-headerLogo.src = logoSrc;
-
 const settings = {
   inputSelector: ".field-input",
   submitButtonSelector: ".popup__save"
