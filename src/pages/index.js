d6017ebe12645b4c33e36ca74b97af268c642fb1
menashekoren <menashekoren@gmail.com>
2021-10-12 11:02:51 +0300

    Hotfix: Remove excess code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,18 @@
 import "../pages/index.css";
 import {
   initialCards,
-  editModal,
-  addCardModal,
   listTemplateSelector,
   profileSelector,
   settings,
   editForm,
   addCardForm,
-  editProfileButton,
-  addCardButton,
-  profileNameValue,
-  profileProfessionValue,
-  profileNameInput,
-  profileProfessionInput,
-  addCardFormModal,
-  cardNameInput,
-  cardLinkInput,
 } from "../scripts/utils/constants";
-import { generateCard, toggleModal } from "../scripts/utils/utils";
+import { generateCard } from "../scripts/utils/utils";
 import Section from "../scripts/components/Section";
 import { PopupWithForm } from "../scripts/components/PopupWithForm";
 import FormValidator from "../scripts/components/FormValidator";
 import PopupWithImage from "../scripts/components/PopupWithImage";
 import UserInfo from "../scripts/components/UserInfo";
-
-
-// export function addFormSubmitListener(modal) {
-//   modal.addEventListener("submit", (evt) => {
-//     evt.preventDefault();
-
-//     if (modal === editModal) {
-//       document.querySelector(".edit-name").textContent = profileNameInput.value;
-//       document.querySelector(".profile-info__profession").textContent =
-//         profileProfessionInput.value;
-//     } else if (modal === addCardModal) {
-//       generateCard({ name: cardNameInput.value, link: cardLinkInput.value });
-//       addCardFormModal.reset();
-//     }
-//     toggleModal(modal);
-//   });
-// }
 
 const editFormValidator = new FormValidator(settings, editForm);
 const addCardFormValidator = new FormValidator(settings, addCardForm);
@@ -55,23 +27,6 @@ const editModalNew = new PopupWithForm(".popup_type_edit", (data) => {
   generateCard(data);
 });
 
-editProfileButton.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  editFormValidator.resetValidation();
-  const profileName = profileNameValue.textContent;
-  profileNameInput.value = profileName;
-  const profileProfession = profileProfessionValue.textContent;
-  profileProfessionInput.value = profileProfession;
-  toggleModal(editModal);
-});
-
-addCardButton.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  addCardFormValidator.resetValidation();
-  addCardFormModal.reset();
-  toggleModal(addCardModal);
-});
-
 editFormValidator.enableValidation();
 addCardFormValidator.enableValidation();
 
@@ -81,9 +36,6 @@ addCardFormValidator.resetValidation();
 imageModalNew.setEventListeners();
 addCardModalNew.setEventListeners();
 editModalNew.setEventListeners();
-
-// addFormSubmitListener(editModal);
-// addFormSubmitListener(addCardModal);
 
 const userInfoPop = new UserInfo(profileSelector, (data) => {
   userInfoPop.setUserInfo(data);
--- src/scripts/utils/utils.js
+++ src/scripts/utils/utils.js
@@ -1,34 +1,6 @@
-import {
-  cardTemplateSelector,
-} from "./constants";
+import { cardTemplateSelector } from "./constants";
 import Card from "../components/Card";
 import { imageModalNew } from "../../pages/index";
-
-export function toggleModal(popup) {
-  const saveButton = popup.querySelector(".popup__save");
-
-  if (saveButton) {
-    saveButton.disabled = true;
-  }
-
-  popup.classList.toggle("popup_opened");
-}
-
-// export function addFormSubmitListener(modal) {
-//   modal.addEventListener("submit", (evt) => {
-//     evt.preventDefault();
-
-//     if (modal === editModal) {
-//       document.querySelector(".edit-name").textContent = profileNameInput.value;
-//       document.querySelector(".profile-info__profession").textContent =
-//         profileProfessionInput.value;
-//     } else if (modal === addCardModal) {
-//       generateCard({ name: cardNameInput.value, link: cardLinkInput.value });
-//       addCardFormModal.reset();
-//     }
-//     toggleModal(modal);
-//   });
-// }
 
 export function generateCard(data) {
   const cardElement = new Card(data, cardTemplateSelector, () => {
