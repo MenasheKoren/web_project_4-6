5f177467b9575d90bc9f585f63011394d8a4b4b4
menashekoren <menashekoren@gmail.com>
2021-11-03 11:49:12 +0200

    Hotfix: fix submit button message

--- src/scripts/utils/utils.js
+++ src/scripts/utils/utils.js
@@ -1,11 +1,14 @@
+import { openedModal, submitButton } from "./constants";
+
 export const customFetch = (url, headers) =>
   fetch(url, headers).then((res) =>
     res.ok ? res.json() : Promise.reject(res.statusText)
   );
 
 export function updateProcessingMessage(message) {
-  const button = document.querySelector(".popup_opened .popup__save");
-  if (button) {
-    button.textContent = message;
+  if (submitButton && openedModal) {
+    submitButton.textContent = message;
+  } else {
+    submitButton.textContent = message;
   }
 }
